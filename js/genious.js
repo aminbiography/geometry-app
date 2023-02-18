1b5273892bd36211c6e26e6a133b5b3b8acc60f5
unknown <biographyamin@gmail.com>
2023-02-18 22:56:00 +0600

    first commit

---
+++ js/genious.js
@@ -0,0 +1,94 @@
+//  Triangle shape
+document.getElementById('btn-01a').addEventListener('click', function multiply(){
+//      Get the values from the input boxes
+        const input01a = document.getElementById("input01a").value;
+        const input02a = document.getElementById("input02a").value;
+    
+//      Multiply the values
+        const result = 0.5 * input01a * input02a;
+    
+//      Set the result in the output box
+        document.getElementById("output01a").value = result;
+        })
+
+        
+
+// rectangle shape
+document.getElementById('btn-01b').addEventListener('click', function multiply(){
+//      Get the values from the input boxes
+        const input01b = document.getElementById("input01b").value;
+        const input02b = document.getElementById("input02b").value;
+
+//      Multiply the values
+        const result = input01b * input02b;
+                  
+//       Set the result in the output box
+        document.getElementById("output01b").value = result;
+        })
+
+
+
+// Parallelogram shape
+document.getElementById('btn-01c').addEventListener('click', function multiply(){
+//      Get the values from the input boxes
+        const input01c = document.getElementById("input01c").value;
+        const input02c = document.getElementById("input02c").value;
+        
+//      Multiply the values
+        const result = input01c * input02c;
+                          
+//       Set the result in the output box
+document.getElementById("output01c").value = result;
+})
+
+
+
+// Rhombus shape
+document.getElementById('btn-01d').addEventListener('click', function multiply(){
+//      Get the values from the input boxes
+        const input01d = document.getElementById("input01d").value;
+        const input02d = document.getElementById("input02d").value;
+            
+//      Multiply the values
+        const result = 0.5 * input01d * input02d;
+            
+//      Set the result in the output box
+        document.getElementById("output01d").value = result;
+        })
+
+
+
+// Pentagon shape
+document.getElementById('btn-01e').addEventListener('click', function multiply(){
+//      Get the values from the input boxes
+        const input01e = document.getElementById("input01e").value;
+        const input02e = document.getElementById("input02e").value;
+                            
+//      Multiply the values
+        const result = 0.5 * input01e * input02e;
+                            
+//      Set the result in the output box
+        document.getElementById("output01e").value = result;
+        })
+
+
+
+// Ellipse shape        
+document.getElementById('btn-01f').addEventListener('click', function multiply(){
+//      Get the values from the input boxes
+        const input01f = document.getElementById("input01f").value;
+        const input02f = document.getElementById("input02f").value;
+                                            
+//      Multiply the values 
+//      pi = 3.141592653589793238 = 3.15
+        const result = 3.15 * input01f * input02f;
+                                            
+//      Set the result in the output box
+        document.getElementById("output01f").value = result;
+        })
+        
+
+
+
+
+
